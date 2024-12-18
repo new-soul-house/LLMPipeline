98b9771c925c215a9c16fa7b3a5a67f479602935
maokangkun <maokangkun@pjlab.org.cn>
2024-12-18 23:19:13 +0800

    update

--- llmpipeline/build_in_prompts/branch_node_prompt.py
+++ llmpipeline/build_in_prompts/branch_node_prompt.py
@@ -1,4 +1,8 @@
-prompt="""Task: Please select the closest item from the items based on the input content and only answer the item id in JSON format: ```json\n{"item_id": "#i"}\n```.
+prompt="""Task: Please select the closest item from the items based on the input content and only answer the item id in JSON format, e.g.
+```json
+{"item_id": "#i"}
+```
+
 Input: {input}
 Items:
 {items}
